e87611acb990015af240bd6c0817a4f48b5c6db0
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-08-18 11:21:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,6 +14,8 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 
 
