19190d868f3b8af027e445da9a8aca8b65683101
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-08-18 11:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Aapko file tabhi milegi jab aap neeche diye gaye channels ko join karenge. Jab aap ye kar lenge, "Try Again" par click karna\n\nThanks You 🙏</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Aapko file tabhi milegi jab aap neeche diye gaye channels ko join karenge. Jab aap ye kar lenge, ""Try Again"" par click karna\n\nThanks You 🙏</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -58,10 +58,10 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only Work For @Team_JXG"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1675155069)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
