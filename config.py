6e45317a1e683aac6d000de36a71461b626c1224
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2024-09-15 14:10:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://JJUSTANIME:teamjxg@cluste
 DB_NAME = os.environ.get("DATABASE_NAME", "My_Anime_Store_Bot")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001619486126"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001621032259"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4000"))
 
